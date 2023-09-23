883770f56d5823d8a9f5c373c2d7096627051005
Iago Vinicius Russi Novaes <34945729+iago010@users.noreply.github.com>
2023-09-23 20:54:03 -0300

    Update desafio.kt
    Completando o código para o desafio de projeto: "Formações Educacionais da DIO."

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,43 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
+// Enum com os níveis
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+// Definindo a classe Aluno
+data class Aluno(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// Definindo a classe Conteudo Educacional
+data class ConteudoEducacional(val nome: String, val duracaoMin: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+// Definindo a classe Formacao com a capacidade de matricular alunos
+data class Formacao(val nome: String, val nivel: Nivel, var conteudosEducacionais: List<ConteudoEducacional>) {
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val matriculados = mutableListOf<Aluno>()
+  
+    fun matricular(aluno: Aluno) {
+        matriculados.add(aluno)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+  // Criação de alunos
+  val aluno1 = Aluno("Alice Ramone")
+  val aluno2 = Aluno("Dom Casmurro")
+  
+  // Criação de conteúdos
+  val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+  val conteudo2 = ConteudoEducacional("Programação Orientada a Objeto", 90)
+  
+  // Criação de formação
+  val formacao = Formacao("Desenvolvimento Android", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+  
+  // Matricular os alunos na formação
+  formacao.matricular(aluno1)
+  formacao.matricular(aluno2)
+  
+  // Verificar lista de matriculados na formação
+  println("Alunos matriculados na formação ${formacao.nome}:")
+  for (aluno in formacao.matriculados){
+      println(aluno.nome)
+  }
 }
